4a25453f567083935229a42aec93e14c9589691b
Supawich Ratchataworrakun <supawich_r@elearning.cmu.ac.th>
2023-02-11 03:01:36 +0700

    Set Up Backend

---
+++ Backend/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+
+const { API_PORT } = process.env;
+const port = process.env.PORT || API_PORT;
+global.__basedir = __dirname;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"),
+    res.header(
+      "Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept"
+    );
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+module.exports = app;

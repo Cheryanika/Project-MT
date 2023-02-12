17ddfe3ad76b86d58afb6ab8e0ed2e320dfccee0
Supawich Ratchataworrakun <supawich_r@elearning.cmu.ac.th>
2023-02-12 12:30:40 +0700

    Set up Frontend/Backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div className="text-center" style={{ backgroundColor: "#3B270C" }}>
+        <br />
+        <img src="/frontend/src/Image/rmutl.webp" weight={82} height={150} />
+        <p className="text-light" style={{ fontSize: 35 }}>
+          แปลบทคัดย่อภาษาไทย-อังกฤษ <br />
+          Abstract Translation Thai-English{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      </div>
+      <div className="container">
+        <p className="text-dark" style={{ fontSize: 25 }}>
+          {" "}
+          ภาษาไทย (Thai)
+        </p>
+        <nav
+          className="navbar navbar-expand-lg navbar-light"
+          style={{ backgroundColor: "#3B270C", maxWidth: 1300, height: 50 }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="container">
+            <select
+              className="form-select form-select-sm"
+              aria-label=".form-select-sm"
+            >
+              <option selected>กรุณาเลือก Machine Translation Model</option>
+              <option value={1}>mt5 model</option>
+              <option value={2}>mBART model</option>
+              <option value={3}>Marian model</option>
+            </select>
+          </div>
+        </nav>
+        <textarea
+          className="form-control"
+          type="text"
+          placeholder="กรุณาใส่บทคัดย่อภาษาไทย"
+          style={{ maxWidth: 1300 }}
+          defaultValue={""}
+        />
+      </div>
+      <div className="text-center">
+        <br />
+        <br />
+        <button
+          type="button"
+          className="btn-custom btn-lg"
+          style={{ maxWidth: 200, height: 50 }}
+        >
+          แปลภาษา
+        </button>
+      </div>
+      <div className="container">
+        <br />
+        <p className="text-dark" style={{ fontSize: 25 }}>
+          {" "}
+          ภาษาอังกฤษ (English)
+        </p>
+        <nav
+          className="navbar navbar-expand-lg navbar-light"
+          style={{ backgroundColor: "#3B270C", maxWidth: 1300, height: 50 }}
+        ></nav>
+        <textarea
+          className="form-control"
+          type="text"
+          readOnly
+          style={{ maxWidth: 1300 }}
+          defaultValue={""}
+        />
+        <br />
+      </div>
+      <nav
+        className="navbar navbar-expand-lg navbar-light"
+        style={{ backgroundColor: "#3B270C", height: 80 }}
+      />
     </div>
   );
 }

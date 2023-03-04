845ad34292e5d19d6e99d531aff11c4ab95cb4eb
Supawich Ratchataworrakun <supawich_r@elearning.cmu.ac.th>
2023-03-04 19:04:01 +0700

    Backend - Firebase, Function (Create)

--- Backend/app.js
+++ Backend/app.js
@@ -1,72 +1,37 @@
-require("dotenv").config();
-require("./config/database").connect();
-
 const express = require("express");
-const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const User = require("./models/HuggingFace");
 const app = express();
 
-// const HuggingFaceRoutes = require("./routes/HuggingfaceRoutes");
-// const HuggingFaceController = require("./controller/HuggingfaceController");
+const admin = require("firebase-admin");
+const credentials = require("./key.json");
 
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
-global.__basedir = __dirname;
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
+});
 
-app.use(bodyParser.json());
+const db = admin.firestore();
+
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"),
-    res.header(
-      "Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept"
-    );
-  next();
+// Create Data in firebase
+app.post("/create/translator", async (req, res) => {
+  try {
+    console.log(req.body);
+    // const id = req.body.English;
+    const translatorJson = {
+      English: req.body.English,
+      Model: req.body.Model,
+      Thai: req.body.Thai,
+    };
+    const response = db.collection("TranslatorData").add(translatorJson);
+    res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
-// const text = { inputs: `${"inputs"} ` };
-
-// var data = { inputs: "Something here" };
-
-// const response = fetch(
-//   `https://api-inference.huggingface.co/models/SigmarAI/mt5-sentence-translator`,
-//   {
-//     headers: {
-//       Authorization: "Bearer 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",
-//     },
-//     method: "POST",
-//     data: JSON.stringify(data),
-//   }
-// );
-
-// const data = await response.json();
-// return data;
-
-async function translateWord(data) {
-  var data = { inputs: `${"inputs"} ` };
-  const response = await fetch(
-    `https://api-inference.huggingface.co/models/SigmarAI/mt5-sentence-translator`,
-    {
-      headers: {
-        Authorization: "Bearer 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",
-      },
-      method: "POST",
-      body: JSON.stringify(data),
-    }
-  );
-  data = await response.json();
-  // data = query({ inputs: "The Output: " }).then((response) => {
-  // });
-  console.log(JSON.stringify(response));
-  return data;
-}
-
-app.use("/api/word", translateWord);
-
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+// Checking Running Port Sever
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Sever is running on PORT ${PORT}.`);
 });
-
-module.exports = app;
--- Backend/routes/HuggingfaceRoutes.js
+++
@@ -1,8 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const HuggingFaceController = require("../controller/HuggingfaceController");
-
-// router.post("/translate", HuggingFaceController);
-
-// module.exports = router;
